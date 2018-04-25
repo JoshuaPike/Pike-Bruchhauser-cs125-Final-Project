47bcebbd4c4e68897f81d508445c7a21780be4eb
Josh Pike <joshuap5@illinois.edu>
2018-04-25 14:04:25 -0500

    Put implements into app.gradle to allow for importing volley and such into the main activity. Made an onClickListener for edit text.

--- app/src/main/java/edu/illinois/cs/cs125/cs125finalprojectpikebruchhauser/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/cs125finalprojectpikebruchhauser/MainActivity.java
@@ -1,13 +1,80 @@
 package edu.illinois.cs.cs125.cs125finalprojectpikebruchhauser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Final Project:Main";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "On create ran");
+
+        requestQueue = Volley.newRequestQueue(this);
+
         super.onCreate(savedInstanceState);
+
+        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
+
+        /*
+         * Set up handlers for each button in our UI. These run when the buttons are clicked.
+         */
+        final TextInputEditText inputText = findViewById(R.id.locationInput);
+        inputText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "City input text clicked");
+                // Method that allows text to be edited.
+            }
+        });
     }
 }
